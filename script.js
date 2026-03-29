73483161e7f0361b03e1d8ddd4b73f648112f70c
Atharva Lonikar <126399397+atharvalonikar95@users.noreply.github.com>
2026-03-29 17:03:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 //your JS code here. If required.
+const age = document.getElementById("age")
+const name = document.getElementById("name")
+const btn = document.getElementById("btn")
+btn.addEventListener("click",(e)=>{
+	e.preventDefault();
+	let ageVal=age.value;
+	let nameVal=name.value;
+	if(ageVal==="" || nameVal===""){
+		alert("Please enter valid details.")
+		return
+	}
+	// alert(age.value) 
+	const promise = new Promise((res,rej)=>{
+	setTimeout(()=>{
+		if(ageVal>18){
+			res("Welcome, . You can vote.")
+		}else{
+			rej("Oh sorry . You aren't old enough.")
+		}
+	},4000)
+	})
+	promise
+			.then(result=>alert(result))
+			.catch(result=>alert(result))
+	age.value=""
+	name.value=""
+})
+
